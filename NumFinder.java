e20ef08a81a06e4f2c3b1c4d5c1ed39ecf55f07d
Diogo Moreira <diogo.dmoreira@gmail.com>
2023-09-21 14:01:56 -0300

    Adicionando NumFinder

---
+++ NumFinder.java
@@ -0,0 +1,14 @@
+public class NumFinder {
+
+  private int smallest = Integer.MAX_VALUE;
+  private int largest = Integer.MIN_VALUE;
+
+  public void find(int[] nums) {
+    for(int n : nums) {
+      if (n < smallest) smallest = n;
+      else if (n > largest) largest = n;
+    }
+  }
+
+  // Gets para menor e maior
+}
